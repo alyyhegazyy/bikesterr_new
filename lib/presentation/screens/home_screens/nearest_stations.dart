7d651e6ff601f1fb95a7fb8ed67e9785c4708bdd
alyhegazy <aly.hegazyy97@gmail.com>
2023-05-11 12:21:57 +0300

    map trial update

--- lib/presentation/screens/home_screens/nearest_stations.dart
+++ lib/presentation/screens/home_screens/nearest_stations.dart
@@ -2,9 +2,9 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
+//import 'package:latlong2/latlong.dart';
 
-//import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class NearestStation extends StatefulWidget {
   const NearestStation({super.key});
@@ -14,21 +14,30 @@ class NearestStation extends StatefulWidget {
 }
 
 class _NearestStationState extends State<NearestStation> {
-  // final Completer<GoogleMapController> _controller =
-  //     Completer<GoogleMapController>();
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FlutterMap(
-        children: [],
-        options: MapOptions(
-          center: LatLng(29.964914, 31.269994),
-          zoom: 13.0,
-        ),
-      ),
+      body: Center(
+          child: GoogleMap(
+        // apiKey:'AIzaSyCFuI0obspjmVCbr4_9c0dYrMfDJ27LtV4',
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        initialCameraPosition:
+            const CameraPosition(target: LatLng(29.964914, 31.269994)),
+      )),
+      // FlutterMap(
+      //   children: [],
+      //   options: MapOptions(
+      //     center: const LatLng(29.964914, 31.269994),
+      //     zoom: 13.0,
+      //   ),
+      // ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        child: Text("test"),
+        child: const Text("test"),
       ),
       // Center(
       //     child: GoogleMap(
